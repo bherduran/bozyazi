a6c919408653b1c6639f024e5971737880cb1e0e
Bilge Han ERDURAN <91484922+bherduran@users.noreply.github.com>
2026-03-19 16:34:22 +0300

    Update comment in Assistant.jsx

--- src/pages/Assistant.jsx
+++ src/pages/Assistant.jsx
@@ -55,7 +55,7 @@ export default function Assistant() {
           parts: [{ text: m.text }],
         }))
 
-   //2.0
+   //deneme
       
       const response = await fetch(
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${import.meta.env.VITE_GEMINI_KEY}`,
