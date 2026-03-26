fa71146f1a5d5801d60133a4f773a3c22121b8f3
Bilge Han ERDURAN <91484922+bherduran@users.noreply.github.com>
2026-03-27 00:25:46 +0300

    Remove base path for GitHub Pages
    Remove base path configuration for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages'e deploy ettiğinde repo adını buraya yaz:
-  base: '/bozyazi/',
+
 })
