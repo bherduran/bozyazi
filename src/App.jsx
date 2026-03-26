8862b2afef4ca1715fc81c314f734cb82f78bd21
Bilge Han ERDURAN <91484922+bherduran@users.noreply.github.com>
2026-03-27 00:26:21 +0300

    Replace HashRouter with BrowserRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@
 // Bunun yerine React Router URL'yi okur ve <Discover /> bileşenini render eder.
 // Bu SPA'nın özüdür: sayfa yenilenmez, sadece bileşen değişir.
 
-import { HashRouter , Routes, Route } from 'react-router-dom'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
 
 import Navbar   from './components/Navbar.jsx'
 import Footer   from './components/Footer.jsx'
@@ -23,9 +23,9 @@ import Visit    from './pages/Visit.jsx'
 
 export default function App() {
   return (
-    // HashRouter : Tüm Router mantığını sarar.
+    // BrowserRouter : Tüm Router mantığını sarar.
     // History API'yi kullanır → URL değişir ama sayfa yenilenmez.
-    <HashRouter >
+    <BrowserRouter >
 
       {/* Navbar her sayfada görünür, Routes dışında */}
       <Navbar />
@@ -51,6 +51,6 @@ export default function App() {
       {/* Footer da her sayfada görünür */}
       <Footer />
 
-    </HashRouter >
+    </BrowserRouter >
   )
 }
